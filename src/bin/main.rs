f40cdefa8894131f61881ead4c65bbe89988fc71
ritz-786 <ritikraj7255@gmail.com>
2020-07-16 18:19:14 +0530

    Multithreaded Web Server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,13 +4,17 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        std::thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
@@ -27,7 +31,7 @@ fn handle_connection(mut stream: TcpStream) {
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(7));
+        thread::sleep(Duration::from_secs(20));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
